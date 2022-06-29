7c7271cca5de02a9512ea483687d67bd17841208
Turu <107145087+TuruEnak@users.noreply.github.com>
2022-06-29 06:05:10 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -238,14 +238,14 @@ kirbotz.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update	    
         if (connection === 'close') {
         let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-            if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); kirbotz.logout(); }
+            if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); startkirbotz(); }
             else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); startkirbotz(); }
             else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); startkirbotz(); }
             else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); kirbotz.logout(); }
-            else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); kirbotz.logout(); }
+            else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); startkirbotz(); }
             else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); startkirbotz(); }
             else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); startkirbotz(); }
-            else kirbotz.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+            else console.log(`Unknown DisconnectReason: ${reason}|${connection}`); startkirbotz()
         }
         console.log('Connected...', update)
     })
